ec5cd2fabc3ec1b37fcf7eab9b96185f335a1e0b
Tomáš Diblík <dibla.tomas@post.cz>
2025-02-24 06:34:17 +0100

    [Docs] Add auth example

---
+++ examples/auth-bearer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/TDiblik/gofiber-swagger/gofiberswagger"
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+	app := fiber.New()
+
+	router := gofiberswagger.NewRouter(app)
+
+	// these do not require auth
+	router.Get("/", nil, HelloHandler)
+	router.Get("/abc", nil, HelloHandler)
+	router.Get("/bca", nil, HelloHandler)
+
+	// create as a variable so we can reuse it
+	security_requirements_docs := &gofiberswagger.SecurityRequirements{{
+		"User API Token": {},
+	}}
+
+	// Option A: specify Security field with your SecurityRequirements for each route
+	router.Get("/behind-auth", &gofiberswagger.RouteInfo{
+		Security: security_requirements_docs,
+	}, HelloHandler)
+
+	gofiberswagger.Register(app, gofiberswagger.Config{
+		Swagger: gofiberswagger.SwaggerConfig{
+			OpenAPI: gofiberswagger.DefaultSwaggerConfig.OpenAPI,
+			Info:    gofiberswagger.DefaultSwaggerConfig.Info,
+			Components: &gofiberswagger.Components{
+				// you have to create
+				SecuritySchemes: map[string]*gofiberswagger.SecuritySchemeRef{
+					"User API Token": {
+						Value: &gofiberswagger.SecurityScheme{
+							Type: "apiKey",
+							Name: "x-user-token", // header name
+							In:   "header",
+						},
+					},
+				},
+			},
+		},
+		// Option B: uncomment the following 2 lines and automatically require auth for all routes
+		// AutomaticallyRequireAuth: true,
+		// RequiredAuth:             security_requirements_docs,
+		SwaggerUI: gofiberswagger.DefaultUIConfig,
+	})
+
+	app.Listen(":3000")
+}
+
+func HelloHandler(c fiber.Ctx) error {
+	return c.SendStatus(200)
+}
